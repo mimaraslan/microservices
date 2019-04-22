842cfc9c171bc46a322245059fa8e25ffc4a4f18
mimaraslan <mimar.aslan@gmail.com>
2019-04-22 21:47:38 +0300

    test 2

--- demo/src/main/java/com/mimaraslan/DemoApplication.java
+++ demo/src/main/java/com/mimaraslan/DemoApplication.java
@@ -9,5 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+
+	public void name() {
+		System.out.println("Test");
+	}
 
 }
