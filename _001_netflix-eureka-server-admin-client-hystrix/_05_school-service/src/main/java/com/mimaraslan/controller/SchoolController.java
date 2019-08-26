30e437d33bf8ed3c6d9860168b75937d2dbd72b7
mimaraslan <mimaraslan@gmail.com>
2019-08-26 20:42:15 +0300

    Microservices
    Microservices

--- _001_netflix-eureka-server-admin-client-hystrix/_05_school-service/src/main/java/com/mimaraslan/controller/SchoolController.java
+++ _001_netflix-eureka-server-admin-client-hystrix/_05_school-service/src/main/java/com/mimaraslan/controller/SchoolController.java
@@ -3,8 +3,6 @@ package com.mimaraslan.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.mimaraslan.service.SchoolService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -17,8 +15,7 @@ public class SchoolController {
 	SchoolService studentService;
 
 	// http://localhost:8015/getSchoolDetails/school1
-	// @RequestMapping(value = "/getSchoolDetails/{schoolname}", method =
-	// RequestMethod.GET)
+	// @RequestMapping(value = "/getSchoolDetails/{schoolname}", method = RequestMethod.GET)
 	@GetMapping(value = "/getSchoolDetails/{schoolname}")
 	public String getStudents(@PathVariable String schoolname) {
 		System.out.println("Going to call student service to get data!");
@@ -26,30 +23,29 @@ public class SchoolController {
 	}
 
 	// http://localhost:8015/test1/school1
-	@RequestMapping(value = "/test1/{schoolname}", method = RequestMethod.GET)
+	@GetMapping(value = "/test1/{schoolname}")
 	public String getStudents1(@PathVariable String schoolname) {
 		System.out.println("test1 Going to call student service to get data!");
 		return studentService.test1(schoolname);
 	}
 
 	// http://localhost:8015/test2/school1
-	@RequestMapping(value = "/test2/{schoolname}", method = RequestMethod.GET)
+	@GetMapping(value = "/test2/{schoolname}")
 	public String getStudents2(@PathVariable String schoolname) {
 		System.out.println("test2 Going to call student service to get data!");
 		return studentService.test2(schoolname);
 	}
 
 //-----------------------------------------------------------------	
-	@RequestMapping(value = "/")
-	@HystrixCommand(fallbackMethod = "fallback_hello", commandProperties = {
+	@HystrixCommand(fallbackMethod = "myShowFallBack", commandProperties = {
 			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000") })
+	@GetMapping(value = "/")
 	public String hello() throws InterruptedException {
 		Thread.sleep(3000);
 		return "Welcome Hystrix";
 	}
 
-	@SuppressWarnings("unused")
-	private String fallback_hello() {
+	private String myShowFallBack() {
 		return "Request fails. It takes long time to response";
 	}
 
