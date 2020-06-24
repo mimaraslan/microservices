3033b240bcf2debddeee5876aeca096c00fcb941
mimaraslan <mimaraslan@gmail.com>
2020-06-24 17:17:15 +0300

    Microservices with Spring Boot and Spring Cloud
    Microservices with Spring Boot and Spring Cloud

---
+++ microservices-demo/service-main-cloud-remote-config-server/readme.txt
@@ -0,0 +1,6 @@
+http://localhost:8888/message
+
+http://localhost:8888/actuator
+
+http://localhost:8888/service-a1/default
+http://localhost:8888/service-a1-default.json
---
+++ microservices-demo/service-main-cloud-remote-config-server/src/main/java/com/example/controller/MyRestResource.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestResource {
+
+	// http://localhost:8888
+	@RequestMapping
+	public String sayHello() {
+		return "Hi, from service-main-cloud-remote-config-server!";
+	}
+
+	// http://localhost:8888/message
+	@RequestMapping(value = "/message")
+	public String getMessage() {
+		return "Hi, from Spring Cloud! - getMessage ";
+	}
+
+}
