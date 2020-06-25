14f1e7ce8f8bdf7d7eabca4eb885f4cc4ba47742
mimaraslan <mimaraslan@gmail.com>
2020-06-25 17:17:43 +0300

    Microservices with Spring Boot and Spring Cloud
    Microservices with Spring Boot and Spring Cloud

---
+++ microservices-demo/service-main-eureka-server/src/main/java/com/example/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
