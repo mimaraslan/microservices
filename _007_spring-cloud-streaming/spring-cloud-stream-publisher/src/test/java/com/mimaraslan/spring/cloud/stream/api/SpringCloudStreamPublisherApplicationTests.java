c07e47e35b415c8b812877c69a7f3b078363cdea
mimaraslan <mimaraslan@gmail.com>
2020-07-15 22:20:48 +0300

    Spring Cloud Stream
    Spring Cloud Stream

---
+++ _007_spring-cloud-streaming/spring-cloud-stream-publisher/src/test/java/com/mimaraslan/spring/cloud/stream/api/SpringCloudStreamPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimaraslan.spring.cloud.stream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
