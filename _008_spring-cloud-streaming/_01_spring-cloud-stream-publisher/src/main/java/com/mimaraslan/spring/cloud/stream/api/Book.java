85aea836d415ea9b5ecb5e835ce1eda0bd5b7420
mimaraslan <mimaraslan@gmail.com>
2021-10-10 13:05:43 +0300

    Spring Cloud
    Spring Cloud

---
+++ _008_spring-cloud-streaming/_01_spring-cloud-stream-publisher/src/main/java/com/mimaraslan/spring/cloud/stream/api/Book.java
@@ -0,0 +1,13 @@
+package com.mimaraslan.spring.cloud.stream.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private int id;
+    private String name;
+}
--- _008_spring-cloud-streaming/_01_spring-cloud-stream-publisher/src/main/java/com/mimaraslan/spring/cloud/stream/api/SpringCloudStreamPublisherApplication.java
+++ _008_spring-cloud-streaming/_01_spring-cloud-stream-publisher/src/main/java/com/mimaraslan/spring/cloud/stream/api/SpringCloudStreamPublisherApplication.java
@@ -2,9 +2,20 @@ package com.mimaraslan.spring.cloud.stream.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@EnableBinding (Source.class)
+@RestController
 @SpringBootApplication
 public class SpringCloudStreamPublisherApplication {
+
+    public Book publishEvent(@RequestBody Book book){
+        return null;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudStreamPublisherApplication.class, args);
