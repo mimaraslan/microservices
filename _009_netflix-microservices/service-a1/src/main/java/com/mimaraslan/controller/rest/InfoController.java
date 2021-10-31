2826dd9ef8c57abccfefd29c99d1b2c74cc2aea1
mimaraslan <mimaraslan@gmail.com>
2021-10-31 23:43:02 +0300

    Java Microservices with Spring Cloud
    Java Microservices with Spring Cloud

---
+++ _009_netflix-microservices/service-a1/src/main/java/com/mimaraslan/controller/rest/InfoController.java
@@ -0,0 +1,35 @@
+package com.mimaraslan.controller.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RequestMapping
+@RestController
+public class InfoController {
+
+	@Value("${app.id}")
+	String instance;
+	
+	@Value("${server.port}")
+	String port;
+	
+	@Value("${config.server.service-a1-key}")
+	private String propertyKey;
+	
+	// http://localhost:9001/info/propertykey
+	@GetMapping("/propertykey")
+	public String fetchProperty() {
+		return propertyKey;
+	}
+	
+	// http://localhost:9001/hello
+	// http://localhost:9001/info
+	@GetMapping({"/hello", "info"})
+	public String sayHello(){
+		return instance + " :  service-a1 : " + port;
+	}
+}
