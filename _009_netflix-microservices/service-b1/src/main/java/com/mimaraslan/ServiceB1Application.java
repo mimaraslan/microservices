b07a1fdf5c72d9292f7ce4f1640d68e20abdf72b
mimaraslan <mimaraslan@gmail.com>
2021-11-09 23:44:44 +0300

    Java Microservices with Spring Cloud
    Java Microservices with Spring Cloud

---
+++ _009_netflix-microservices/service-b1/src/main/java/com/mimaraslan/ServiceB1Application.java
@@ -0,0 +1,18 @@
+package com.mimaraslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceB1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceB1Application.class, args);
+	}
+}
