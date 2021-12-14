aff81d80db4f1de9470b5445c880174d15173c3a
mimaraslan <mimaraslan@gmail.com>
2021-12-14 05:29:04 +0300

    Java Microservices with Spring Cloud
    Java Microservices with Spring Cloud

---
+++ _009_netflix-microservices/main-service-spring-cloud-zuul-api-gateway-server/src/main/java/com/mimaraslan/filter/ZuulErrorFilter.java
@@ -0,0 +1,38 @@
+package com.mimaraslan.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulErrorFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext requestContext = RequestContext.getCurrentContext();
+		//requestContext.getThrowable();
+		HttpServletRequest httpServletRequest = requestContext.getRequest();
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return FilterConstants.ERROR_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return FilterConstants.SEND_ERROR_FILTER_ORDER - 1;
+	}
+
+}
