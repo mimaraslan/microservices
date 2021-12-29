d9938f6dbefa9a95f4801c9cadf922cd7fd89e2e
mimaraslan <mimaraslan@gmail.com>
2021-12-29 05:30:44 +0300

    Java Microservices with Spring Cloud
    Java Microservices with Spring Cloud

--- _009_netflix-microservices/service-a1/src/main/java/com/mimaraslan/ServiceA1Application.java
+++ _009_netflix-microservices/service-a1/src/main/java/com/mimaraslan/ServiceA1Application.java
@@ -3,8 +3,11 @@ package com.mimaraslan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableHystrixDashboard
 @EnableCircuitBreaker
@@ -12,8 +15,13 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 public class ServiceA1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceA1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceA1Application.class, args);
+    }
 
+   // @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
