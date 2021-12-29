e25906012d7f30937e681d9a4256a245ca3c9b79
mimaraslan <mimaraslan@gmail.com>
2021-12-29 05:30:56 +0300

    Java Microservices with Spring Cloud
    Java Microservices with Spring Cloud

--- _009_netflix-microservices/service-a1/src/main/java/com/mimaraslan/controller/rest/MyMicroservice1Controller.java
+++ _009_netflix-microservices/service-a1/src/main/java/com/mimaraslan/controller/rest/MyMicroservice1Controller.java
@@ -1,40 +1,28 @@
 package com.mimaraslan.controller.rest;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.mimaraslan.model.MyMicroservice1;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import com.mimaraslan.model.MyMicroservice1;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class MyMicroservice1Controller {
-	
+
 	@Autowired
 	RestTemplate restTemplate;
 
-	@Bean
-	@LoadBalanced
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-	
-	private static final Map<Integer, MyMicroservice1> myMicroervice1Data = new HashMap<Integer, MyMicroservice1>() {
-
-		private static final long serialVersionUID = 1L;{
-			put(1, new MyMicroservice1(1, "myMicroervice1-Object1"));
-			put(2, new MyMicroservice1(2, "myMicroervice1-Object2"));
+	private static final Map<Integer, MyMicroservice1> myMicroservice1Data = new HashMap<>() {
+		{
+			put(1, new MyMicroservice1(1, "myMicroservice1-Object1"));
+			put(2, new MyMicroservice1(2, "myMicroservice1-Object2"));
 		}
 	};
 
@@ -42,14 +30,14 @@ public class MyMicroservice1Controller {
 	// http://localhost:9001/service1/1
 	@HystrixCommand
 	@GetMapping(value = "/service1/{myTestId}")
-	public MyMicroservice1 getMyMicroervice1Details(@PathVariable int myTestId) {
+	public MyMicroservice1 getMyMicroservice1Details(@PathVariable int myTestId) {
 
-		MyMicroservice1 myMicroervice1 = myMicroervice1Data.get(myTestId);
-		if (myMicroervice1 == null) {
-			myMicroervice1 = new MyMicroservice1(0, "N/A");
+		MyMicroservice1 myMicroservice1 = myMicroservice1Data.get(myTestId);
+		if (myMicroservice1 == null) {
+			myMicroservice1 = new MyMicroservice1(0, "N/A");
 		}
-		System.out.println("Getting MyMicroervice1 details for " + myTestId);
-		return myMicroervice1;
+		System.out.println("Getting MyMicroservice1 details for " + myTestId);
+		return myMicroservice1;
 	}
 	
 	
@@ -58,7 +46,7 @@ public class MyMicroservice1Controller {
 	@HystrixCommand(fallbackMethod = "fallbackMethodForService1")
 	@RequestMapping(value = "/service1toService2/{myTestId}", method = RequestMethod.GET)
 	public String getFromService1toService2Info(@PathVariable int myTestId) {
-	
+
 		String response = restTemplate
 				.exchange("http://service-b1/service2/"+myTestId
 				, HttpMethod.GET
@@ -77,6 +65,5 @@ public class MyMicroservice1Controller {
 	public String fallbackMethodForService1(int myTestId) {
 		return "Fallback response:: No MyMicroservice2 details available temporarily :"+ myTestId;
 	}
-	
-	
+
 }
